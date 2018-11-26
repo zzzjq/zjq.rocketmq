a8959a87e183e80641f1c2e2c85fd2d939759951
zhangjingqi <zhangjingqiyy@sina.com>
2018-11-26 15:27:03 +0800

    zjq.rocketmq

---
+++ src/test/java/zjq/work/main/Main.java
@@ -0,0 +1,19 @@
+package zjq.work.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 
+ * @author zhangjq
+ * @date 2018年11月26日下午3:16:51
+ *
+ **/
+public class Main {
+	public static void main(String[] args) throws Exception{
+		ApplicationContext a = new ClassPathXmlApplicationContext(
+				new String[] {"rocketmq/rocketmq.consumer.xml",
+						"rocketmq/rocketmq.producer.xml"});	
+		
+	}
+}
